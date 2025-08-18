a75d611655af49020d68e9da5144c44b984c22df
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-08-18 19:18:13 -0300

    Primer commit : Booking System

---
+++ src/main/java/com/aleprimo/Booking_System_App/BookingSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.aleprimo.Booking_System_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemAppApplication.class, args);
+	}
+
+}
