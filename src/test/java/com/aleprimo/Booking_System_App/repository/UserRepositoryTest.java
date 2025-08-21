a54ab924a003e62798826ec9254da2336dd8ec7c
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-08-21 18:18:33 -0300

    Correccion final de userRepoTest

--- src/test/java/com/aleprimo/Booking_System_App/repository/UserRepositoryTest.java
+++ src/test/java/com/aleprimo/Booking_System_App/repository/UserRepositoryTest.java
@@ -3,16 +3,19 @@ package com.aleprimo.Booking_System_App.repository;
 
 import com.aleprimo.Booking_System_App.entity.User;
 import com.aleprimo.Booking_System_App.entity.enums.Role;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@ActiveProfiles("test")
 @DataJpaTest
+@RequiredArgsConstructor
 class UserRepositoryTest {
 
     @Autowired
