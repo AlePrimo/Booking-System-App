46b25c07b6a811755a92868c95bfb82d3711f662
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-08-23 18:33:30 -0300

    Agregado de clase controller test para offering

---
+++ src/test/java/com/aleprimo/Booking_System_App/controller/OfferingControllerTest.java
@@ -0,0 +1,131 @@
+package com.aleprimo.Booking_System_App.controller;
+
+import com.aleprimo.Booking_System_App.controller.offering.OfferingController;
+import com.aleprimo.Booking_System_App.dto.offering.OfferingRequestDTO;
+import com.aleprimo.Booking_System_App.dto.offering.OfferingResponseDTO;
+import com.aleprimo.Booking_System_App.entity.Offering;
+import com.aleprimo.Booking_System_App.entity.User;
+import com.aleprimo.Booking_System_App.entity.enums.Role;
+import com.aleprimo.Booking_System_App.mapper.offering.OfferingMapper;
+
+import com.aleprimo.Booking_System_App.service.OfferingService;
+import com.aleprimo.Booking_System_App.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(OfferingController.class)
+@ActiveProfiles("test")
+class OfferingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private OfferingService offeringService;
+
+     @MockitoBean
+     private UserService userService;
+
+    @MockitoBean
+    private OfferingMapper offeringMapper;
+
+    private Offering offering;
+    private OfferingRequestDTO requestDTO;
+    private OfferingResponseDTO responseDTO;
+    private User provider;
+
+    @BeforeEach
+    void setUp() {
+
+        provider = User.builder()
+                .name("VANDALAY")
+                .email("vandalay@mail")
+                .password("vandalay124")
+                .roles(Set.of(Role.PROVIDER))
+                .build();
+
+
+        offering = Offering.builder()
+                .id(1L)
+                .name("Test Offering")
+                .description("Desc")
+                .provider(provider)
+                .durationMinutes(15)
+                .price(BigDecimal.valueOf(120.0))
+                .build();
+
+        requestDTO = OfferingRequestDTO.builder()
+                .name("Test Offering")
+                .description("Desc")
+                .price(BigDecimal.valueOf(120.0))
+                .build();
+
+        responseDTO = OfferingResponseDTO.builder()
+                .id(1L)
+                .name("Test Offering")
+                .description("Desc")
+                .price(BigDecimal.valueOf(120.0))
+                .build();
+    }
+
+    @Test
+    void testCreateOffering() throws Exception {
+        when(offeringMapper.toEntity(any(OfferingRequestDTO.class))).thenReturn(offering);
+        when(offeringService.createOffering(any(Offering.class))).thenReturn(offering);
+        when(offeringMapper.toDTO(any(Offering.class))).thenReturn(responseDTO);
+
+        mockMvc.perform(post("/api/offerings")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L));
+    }
+
+    @Test
+    void testGetOfferingById() throws Exception {
+        when(offeringService.getOfferingById(1L)).thenReturn(Optional.of(offering));
+        when(offeringMapper.toDTO(any(Offering.class))).thenReturn(responseDTO);
+
+        mockMvc.perform(get("/api/offerings/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Test Offering"));
+    }
+
+    @Test
+    void testGetAllOfferings() throws Exception {
+        when(offeringService.getAllOfferings(any(Pageable.class)))
+                .thenReturn(new PageImpl<>(List.of(offering), PageRequest.of(0, 10), 1));
+        when(offeringMapper.toDTO(any(Offering.class))).thenReturn(responseDTO);
+
+        mockMvc.perform(get("/api/offerings?page=0&size=10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].name").value("Test Offering"));
+    }
+}
