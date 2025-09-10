128a964764cd1cb41ed99c420f3f44617c3d0ee0
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-09-10 18:48:06 -0300

    Agregado de clases UserDetail para implementar JWT

---
+++ src/main/java/com/aleprimo/Booking_System_App/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.aleprimo.Booking_System_App.security;
+
+import com.aleprimo.Booking_System_App.entity.User;
+import com.aleprimo.Booking_System_App.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con email: " + email));
+        return new CustomUserDetails(user);
+    }
+}
