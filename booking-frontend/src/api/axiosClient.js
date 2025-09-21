87fd47974b5214e56efafc0ac99dfda5edde2de4
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-09-21 18:31:02 -0300

    Actualizacion de la ruta en axiosClient

--- booking-frontend/src/api/axiosClient.js
+++ booking-frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // 👈 cambia si tu backend corre en otro puerto/ruta
+  baseURL: "http://localhost:8080", // 👈 cambia si tu backend corre en otro puerto/ruta
   headers: {
     "Content-Type": "application/json",
   },
